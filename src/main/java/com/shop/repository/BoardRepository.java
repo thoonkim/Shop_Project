fa10d163dd14026408e5202c11ffc572852ef6d1
thoonkim <thoon.kim12@gmail.com>
2022-03-15 13:55:23 +0900

    220315. 복구

--- src/main/java/com/shop/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shop.repository;
-
-import com.shop.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
